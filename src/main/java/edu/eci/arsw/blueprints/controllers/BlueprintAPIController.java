ef329439ab013326719174a351ff84db2dee49c5
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-09-14 14:42:59 -0500

    Parte Uno punto 4

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -7,10 +7,14 @@ package edu.eci.arsw.blueprints.controllers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import edu.eci.arsw.blueprints.controllers.model.Blueprint;
 import edu.eci.arsw.blueprints.controllers.services.BlueprintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+@RestController //los @RestController anotación en Spring es esencialmente una combinación de @Controller y @ResponseBody. Esta anotación se agregó durante la Spring 4.0 para eliminar la redundancia de declarar el @ResponseBody anotación en su controlador.
+@RequestMapping(value = "/blueprints") //Se utiliza para asignar solicitudes web a clases de controlador específicas y/o métodos de controlador. @RequestMappingse puede aplicar a la clase de controlador, así como a los métodos.
 public class BlueprintAPIController {
+
     @Autowired
     BlueprintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecursoXX(){ //ResponseEntity<?> Envolver la respuesta y además se puede agregar unos parámetros.
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);//HttpStatus.ACCEPTED 202 Accepted. https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);//HttpStatus.NOT_FOUND - Cuando el recurso no existe
+        }
+    }
+
 }
 
--- src/main/java/edu/eci/arsw/blueprints/controllers/UI/Main.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/UI/Main.java
@@ -10,28 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
-    public static void main(String arg[]) throws BlueprintPersistenceException, BlueprintNotFoundException {
+    public static void main_spring(String arg[]) throws BlueprintPersistenceException, BlueprintNotFoundException {
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         BlueprintsServices bps = ac.getBean(BlueprintsServices.class);
-
-        Point[] pts = new Point[]{new Point(2,2),new Point(31,31)};
-        Point[] pts2 = new Point[]{new Point(5,5),new Point(3,3)};
-        Point[] pts3 = new Point[]{new Point(5,5),new Point(5,5)};
-        Point[] pts4 = new Point[]{new Point(65,23),new Point(67,98)};
-
-        Blueprint bp = new Blueprint("Deivid", "PruebaUno", pts);
-        Blueprint bp2 = new Blueprint("Cristian", "PruebaDos", pts2);
-        Blueprint bp3 = new Blueprint("Cristian", "PruebaTres", pts3);
-        Blueprint bp4 = new Blueprint("Pepito", "PruebaCuatro", pts4);
-
-        bps.addNewBlueprint(bp);
-        bps.addNewBlueprint(bp2);
-        bps.addNewBlueprint(bp3);
-        bps.addNewBlueprint(bp4);
-
-        System.out.println(bps.getAllBlueprints());
-        System.out.println(bps.getBlueprint("Deivid","PruebaUno"));
-        System.out.println(bps.getBlueprintsByAuthor("Cristian"));
-
     }
 }
